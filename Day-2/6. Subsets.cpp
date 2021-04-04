5e3b2406dc1935e88ea06efc038e8e6d38cdbd11
shumbul <shumbul.181co152@nitk.edu.in>
2021-04-04 14:08:00 +0530

    Day-2

--- Day-2/6. Subsets.cpp
+++ Day-2/6. Subsets.cpp
@@ -6,19 +6,19 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+
         vector<vector<int>> ans;
         ans.push_back({});
-        
-        for(int num: nums) {
+
+        for (int num : nums) {
             vector<vector<int>> newsub;
             vector<int> thissub;
-            for(auto old:ans) {
-                thissub=old;
+            for (auto old : ans) {
+                thissub = old;
                 thissub.push_back(num);
                 newsub.push_back(thissub);
             }
-            for(auto a:newsub) {
+            for (auto a : newsub) {
                 ans.push_back(a);
             }
         }
@@ -32,21 +32,22 @@ class Solution {
     vector<vector<int>> ans;
     int k;
     void backtrack(vector<int> &nums, int first, vector<int> curr) {
-        if(curr.size()==k) {
+        if (curr.size() == k) {
             ans.push_back(curr);
+            return;
         }
-        for(int i=first;i<nums.size();i++) {
+        for (int i = first; i < nums.size(); i++) {
             curr.push_back(nums[i]);
-            backtrack(nums, i+1, curr);
+            backtrack(nums, i + 1, curr);
             curr.pop_back();
         }
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+
         vector<int> curr;
-        for(k=0;k<nums.size()+1;k++){
-            curr={};
+        for (k = 0; k < nums.size() + 1; k++) {
+            curr = {};
             backtrack(nums, 0, curr);
         }
         return ans;
